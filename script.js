fcb8033db956bcabea74694b894d970ec414ad39
Glitch (kris-pilcher) <none>
2018-11-13 20:14:58 +0000

    🍴💿 Checkpoint ./style.css:2130580/2199 ./index.html:2130580/2514 ./script.js:2130580/215

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ $(".hover").mouseleave(
     $(this).removeClass("hover");
   }
 );
+
+$(function() {
+  $('.intro').addClass('go');
+
+  $('.reload').click(function() {
+    $('.intro').removeClass('go').delay(200).queue(function(next) {
+      $('.intro').addClass('go');
+      next();
+    });
+
+  });
+})
