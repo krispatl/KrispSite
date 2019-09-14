37ee8ae1d6c0663ac362852f81c2308c1534b47e
Glitch (kris-pilcher) <none>
2019-09-14 04:27:08 +0000

    😂💨 Checkpoint ./script.js:2130580/634 ./style.css:2130580/446 ./index.html:2130580/1192

--- script.js
+++ script.js
@@ -2,6 +2,36 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
 console.log('hi');
 $(".hover").mouseleave(
   function () {
